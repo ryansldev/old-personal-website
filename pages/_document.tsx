8a9962c0082401340bde149d4a97753180a1d5c4
ryansldev <ryanlima1309@gmail.com>
2022-01-08 12:09:46 -0300

    :adhesive_bandage: Add description in the _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+          <meta name="description" content="Ryan Lima - UI/UX Designer e desenvolvedor FullStack"></meta>
         </Head>
         <body>
           <Main />
