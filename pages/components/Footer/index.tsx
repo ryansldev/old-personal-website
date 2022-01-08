a6a9b4823d69e5b72fb816747caeef39686a5b0b
ryansldev <ryanlima1309@gmail.com>
2022-01-08 08:53:16 -0300

    :sparkles: Done footer

---
+++ pages/components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.css';
+import Link from 'next/link';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h1>Ryan Lima</h1>
+      <div className={styles.networks}>
+        <Link href="https://github.com/ryansldev" passHref={true}>
+          <a target="_blank">
+            <FaGithub size={20} />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/ryansldev/" passHref={true}>
+          <a target="_blank">
+            <FaLinkedin size={20} />
+          </a>
+        </Link>
+        <Link href="https://twitter.com/ryansldev" passHref={true}>
+          <a target="_blank">
+            <FaTwitter size={20} />
+          </a>
+        </Link>
+      </div>
+      <span className={styles.copyright}>
+        &#169; Todos os direitos reservados, Ryan Lima
+      </span>
+    </footer>
+  )
+}
