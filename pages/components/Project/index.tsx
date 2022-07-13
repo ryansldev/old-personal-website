59db9d2ba691eeee7f5c26cb80dd2b1c908d0af3
ryansldev <ryanlima1309@gmail.com>
2022-07-13 17:58:13 -0300

    feat: add mockups to project component

--- pages/components/Project/index.tsx
+++ pages/components/Project/index.tsx
@@ -5,37 +5,71 @@ import Link from "next/link";
 import styles from "./styles.module.css";
 
 type ProjectProps = {
-  image: string;
+  image?: string;
   children: ReactNode;
   projectLink?: string;
   imageAt?: "left" | "right";
+  mockups?: {
+    url: string;
+  }[];
 };
 
 const Project: FC<ProjectProps> = ({
-  image = "left",
-  imageAt,
+  image,
+  imageAt = "left",
   projectLink = "https://github.com/ryansldev",
+  mockups,
   children,
 }) => {
   return (
     <div className={styles.projectContainer}>
-      <div
-        className={styles.project}
-        style={{
-          flexDirection: `${imageAt === "right" ? "row-reverse" : "row"}`,
-        }}
-      >
-        <Link href={projectLink} passHref>
-          <a
-            className={styles.background}
-            style={{ backgroundImage: `url(${image})` }}
-            target="_blank"
-          ></a>
-        </Link>
-        <div className={styles.content}>
-          <div className={styles.text}>{children}</div>
+      <div className="container">
+        <div
+          className={styles.project}
+          style={{
+            flexDirection: `${
+              image ? (imageAt === "right" ? "row-reverse" : "row") : "column"
+            }`,
+            minHeight: `${image ? "100vh" : "100%"}`,
+          }}
+        >
+          {image && (
+            <Link href={projectLink} passHref>
+              <a
+                className={styles.background}
+                style={{ backgroundImage: `url(${image})` }}
+                target="_blank"
+              ></a>
+            </Link>
+          )}
+          <div
+            className={styles.content}
+            style={{
+              marginTop: `${image ? "0px" : "5em"}`,
+              maxWidth: `${image ? "77vw" : "50vw"}`,
+            }}
+          >
+            <div className={styles.text}>{children}</div>
+          </div>
         </div>
       </div>
+      {mockups && mockups?.length > 0 && (
+        <div className={styles.mockups}>
+          {mockups.map((mockup, key) => (
+            <div key={key}>
+              <Link href={projectLink} passHref>
+                <a
+                  target="_blank"
+                  className={styles.mockup}
+                  style={{
+                    backgroundImage: `linear-gradient(180deg, #0F0F0F 9.58%, rgba(15, 15, 15, 0) 52.24%, rgba(15, 15, 15, 0) 63.58%, #0F0F0F 96.71%), url(${mockup.url})`,
+                  }}
+                ></a>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
