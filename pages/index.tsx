432f9a8338322fa786708893117a4051a51c0007
ryansldev <ryanlima1309@gmail.com>
2022-07-13 17:58:38 -0300

    feat: using mockups of project component

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,129 +64,130 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className={styles.projects} id="projects">
-          <div className={styles.container}>
-            <h1>PROJETOS</h1>
-            <p>Projetos que desenvolvi ao longo da minha carreira</p>
+          <h1>PROJETOS</h1>
+          <p>Projetos que desenvolvi ao longo da minha carreira</p>
 
-            <Project
-              projectLink="https://github.com/ryansldev/world-wide-dev"
-              image="https://unsplash.com/photos/Q1p7bh3SHj8/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MzB8fGNvbm5lY3Rpb258ZW58MHx8fHwxNjU3MTkzODE5&force=true&w=1920"
-            >
-              <p>
-                <strong>WWD</strong> foi um projeto desenvolvido por mim a
-                alguns anos que será reformulado em outro projeto chamado
-                <strong> dev.radar.</strong>
-              </p>
-              <p>
-                O intuito desse projeto é conectar desenvolvedores ao redor do
-                mundo de uma forma simples e prática.
-              </p>
+          <Project
+            projectLink="https://github.com/ryansldev/world-wide-dev"
+            image="https://unsplash.com/photos/Q1p7bh3SHj8/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MzB8fGNvbm5lY3Rpb258ZW58MHx8fHwxNjU3MTkzODE5&force=true&w=1920"
+          >
+            <p>
+              <strong>WWD</strong> foi um projeto desenvolvido por mim a alguns
+              anos que será reformulado em outro projeto chamado
+              <strong> dev.radar.</strong>
+            </p>
+            <p>
+              O intuito desse projeto é conectar desenvolvedores ao redor do
+              mundo de uma forma simples e prática.
+            </p>
 
-              <p>
-                Para conferir o projeto,{" "}
-                <Link
-                  href="https://github.com/ryansldev/world-wide-dev"
-                  passHref
-                >
-                  <a target="_blank">clique aqui</a>
-                </Link>
-              </p>
-            </Project>
+            <p>
+              Para conferir o projeto,{" "}
+              <Link href="https://github.com/ryansldev/world-wide-dev" passHref>
+                <a target="_blank">clique aqui</a>
+              </Link>
+            </p>
+          </Project>
 
-            <Project
-              image="https://unsplash.com/photos/_nm_mZ4Cs2I/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU3MjAwNjQ0&force=true&w=1920"
-              projectLink="https://github.com/ryansldev/letmeask"
-              imageAt="right"
-            >
-              <p>
-                O <strong>Letmeask</strong> foi um projeto desenvolvido em um
-                evento chamado <strong>NLW</strong>, nesse evento que acontece
-                todos os anos sempre criamos uma aplicação do{" "}
-                <strong>total zero</strong> em uma semana com as melhores stacks
-                de todo mercado.
-              </p>
-              <p>
-                Em uma das milhares de vezes que participei, criamos uma
-                plataforma para o desenvolvimento de perguntas e respostas
-                online em uma live stream para facilitar o criador de conteúdo a
-                organizar e categorizar as perguntas.
-              </p>
+          <Project
+            image="https://unsplash.com/photos/_nm_mZ4Cs2I/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU3MjAwNjQ0&force=true&w=1920"
+            projectLink="https://github.com/ryansldev/letmeask"
+            imageAt="right"
+          >
+            <p>
+              O <strong>Letmeask</strong> foi um projeto desenvolvido em um
+              evento chamado <strong>NLW</strong>, nesse evento que acontece
+              todos os anos sempre criamos uma aplicação do{" "}
+              <strong>total zero</strong> em uma semana com as melhores stacks
+              de todo mercado.
+            </p>
+            <p>
+              Em uma das milhares de vezes que participei, criamos uma
+              plataforma para o desenvolvimento de perguntas e respostas online
+              em uma live stream para facilitar o criador de conteúdo a
+              organizar e categorizar as perguntas.
+            </p>
 
-              <p>
-                Para conferir o projeto,{" "}
-                <Link href="https://github.com/ryansldev/letmeask" passHref>
-                  <a target="_blank">clique aqui</a>
-                </Link>
-              </p>
-            </Project>
+            <p>
+              Para conferir o projeto,{" "}
+              <Link href="https://github.com/ryansldev/letmeask" passHref>
+                <a target="_blank">clique aqui</a>
+              </Link>
+            </p>
+          </Project>
 
-            <Project image="https://unsplash.com/photos/o-ubWHV29Uk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8bm90ZXxlbnwwfHx8fDE2NTcyMTE1MzA&force=true&w=1920">
-              <p>
-                <strong>Feedget Widget</strong> foi outra aplicação desenvolvida
-                na <strong>NLW</strong> e essa aplicação consistiu em um widget
-                que pode ser utilizado em qualquer sistema para receber
-                feedbacks.
-              </p>
+          <Project
+            // image="https://unsplash.com/photos/o-ubWHV29Uk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8bm90ZXxlbnwwfHx8fDE2NTcyMTE1MzA&force=true&w=1920"
+            mockups={[
+              {
+                url: "/assets/feedget-iphone-mockup.webp",
+              },
+              {
+                url: "/assets/feedget-ipad-mockup.webp",
+              },
+            ]}
+          >
+            <p>
+              <strong>Feedget Widget</strong> foi outra aplicação desenvolvida
+              na <strong>NLW</strong> e essa aplicação consistiu em um widget
+              que pode ser utilizado em qualquer sistema para receber feedbacks.
+            </p>
 
-              <p>
-                A aplicação também foi colocada no ar por mim, então você pode
-                verificar online {""}
-                <Link href="https://feedget-web-mauve.vercel.app/" passHref>
-                  <a target="_blank">clicando aqui</a>
-                </Link>
-              </p>
+            <p>
+              A aplicação também foi colocada no ar por mim, então você pode
+              verificar online {""}
+              <Link href="https://feedget-web-mauve.vercel.app/" passHref>
+                <a target="_blank">clicando aqui</a>
+              </Link>
+            </p>
 
-              <p>
-                Projeto no GitHub: {""}
-                <Link href="https://github.com/ryansldev/feedget-web" passHref>
-                  <a target="_blank">Web</a>
-                </Link>
-                {""} e {""}
-                <Link
-                  href="https://github.com/ryansldev/feedget-server"
-                  passHref
-                >
-                  <a target="_blank">API</a>
-                </Link>
-              </p>
-            </Project>
+            <p>
+              Projeto no GitHub: {""}
+              <Link href="https://github.com/ryansldev/feedget-web" passHref>
+                <a target="_blank">Web</a>
+              </Link>
+              {""} e {""}
+              <Link href="https://github.com/ryansldev/feedget-server" passHref>
+                <a target="_blank">API</a>
+              </Link>
+            </p>
+          </Project>
 
-            <Project
-              image="https://unsplash.com/photos/DOu3JJ3eLQc/download?force=true&w=1920"
-              projectLink="https://github.com/ryansldev/event-platform"
-              imageAt="right"
-            >
-              <p>
-                <strong>Event platform</strong> foi uma aplicação desenvolvida
-                no <strong>Ignite Lab</strong> que é da mesma empresa
-                responsável pela NLW, a {""}
-                <Link href="https://www.rocketseat.com.br/" passHref>
-                  <a target="_blank">Rocketseat</a>
-                </Link>
-              </p>
+          <Project
+            image="https://unsplash.com/photos/DOu3JJ3eLQc/download?force=true&w=1920"
+            projectLink="https://github.com/ryansldev/event-platform"
+            imageAt="right"
+          >
+            <p>
+              <strong>Event platform</strong> foi uma aplicação desenvolvida no{" "}
+              <strong>Ignite Lab</strong> que é da mesma empresa responsável
+              pela NLW, a {""}
+              <Link href="https://www.rocketseat.com.br/" passHref>
+                <a target="_blank">Rocketseat</a>
+              </Link>
+            </p>
 
-              <p>
-                Desta vez tivemos uma plataforma de eventos, para o usuário é
-                simples, ele entra colocando seu e-mail e está tudo pronto,
-                todas as aulas estarão disponíveis.
-              </p>
+            <p>
+              Desta vez tivemos uma plataforma de eventos, para o usuário é
+              simples, ele entra colocando seu e-mail e está tudo pronto, todas
+              as aulas estarão disponíveis.
+            </p>
 
-              <p>
-                Para o instrutor, basta cadastrar a aula com um vídeo hospedado
-                no YouTube, data de liberação, título, descrição e etc. Na data
-                cadastrada o conteúdo ficará disponível na plataforma.
-              </p>
+            <p>
+              Para o instrutor, basta cadastrar a aula com um vídeo hospedado no
+              YouTube, data de liberação, título, descrição e etc. Na data
+              cadastrada o conteúdo ficará disponível na plataforma.
+            </p>
 
-              <p>
-                Caso queira conferir o projeto,{" "}
-                <Link href="https://github.com/ryansldev/event-platform">
-                  <a target="_blank">clique aqui</a>
-                </Link>{" "}
-                {""}
-                ou na imagem.
-              </p>
-            </Project>
-          </div>
+            <p>
+              Caso queira conferir o projeto,{" "}
+              <Link href="https://github.com/ryansldev/event-platform">
+                <a target="_blank">clique aqui</a>
+              </Link>{" "}
+              {""}
+              ou na imagem.
+            </p>
+          </Project>
         </div>
         <div className={styles.network}>
           <div className={styles.container}>
